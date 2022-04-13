8a705c6f10be5377fad551f7a2728f0b09538633
Endersson <enderalonso24@gmail.com>
2022-04-13 15:53:57 -0600

    Calculo funcional

--- CalculadoraINSS/MainPage.xaml.cs
+++ CalculadoraINSS/MainPage.xaml.cs
@@ -10,9 +10,42 @@ namespace CalculadoraINSS
 {
     public partial class MainPage : ContentPage
     {
+        double factorBasico = 0.20;
+        double factorAplicacion = 0.010;
+        int semanaAnual = 52;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        public void Button_Clicked(object sender, EventArgs e)
+        {
+            var semanasUsuario = Convert.ToInt32(Semanas.Text);
+            var salarioUsuario = Convert.ToDouble(Salario.Text);
+
+
+            Resultado.Text = Convert.ToString(pensionJubilacion(semanasUsuario, salarioUsuario));
+        }
+
+        public double pensionJubilacion(int semanasU, double salario)
+        {
+            double excesoSemanas = semanasU - 150;
+            double semanas = excesoSemanas / semanaAnual;
+            double factorAnual = semanas * factorAplicacion;
+            double tasaReemplazo = factorAnual + factorBasico;
+            double pension = tasaReemplazo * salario;
+
+            //que no sea menor de 5000
+
+            if (pension < 5000)
+            {
+                pension = 5000;
+            }
+            
+
+
+            return pension;
         }
     }
 }
--- CalculadoraINSS/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ CalculadoraINSS/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,20 @@ namespace CalculadoraINSS {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry Salario;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry Semanas;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry Resultado;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            Salario = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "Salario");
+            Semanas = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "Semanas");
+            Resultado = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "Resultado");
         }
     }
 }
