d00335cc155b391def3cf2cd951b634fd21e8eb8
Endersson <enderalonso24@gmail.com>
2022-04-13 16:31:26 -0600

    Añadidas todas las funcionalidades de calculo

--- CalculadoraINSS/MainPage.xaml.cs
+++ CalculadoraINSS/MainPage.xaml.cs
@@ -20,12 +20,55 @@ namespace CalculadoraINSS
         }
 
         public void Button_Clicked(object sender, EventArgs e)
+        {           
+            try
+            {
+                var semanasUsuario = Convert.ToInt32(Semanas.Text);
+                var salarioUsuario = Convert.ToDouble(Salario.Text);
+
+                // dteerminar que funcion se va a llamar...
+                if (semanasUsuario >= 250 && semanasUsuario <= 749)
+                {
+                    Resultado.Text = Convert.ToString(pensionReducida(semanasUsuario));
+                }
+                else if (semanasUsuario >= 750)
+                {
+                    Resultado.Text = Convert.ToString(pensionJubilacion(semanasUsuario, salarioUsuario));
+                }
+                else
+                {
+                    Resultado.Text = "Las semanas que usted cotizó no son suficientes para optar a una pensión";
+                }
+            }
+            catch (Exception)
+            {
+                Resultado.Text = "Ocurrió un error inesperado";
+            }
+
+        }
+
+        public double pensionReducida(int semanasU)
         {
-            var semanasUsuario = Convert.ToInt32(Semanas.Text);
-            var salarioUsuario = Convert.ToDouble(Salario.Text);
+            double pension = 0;
 
-
-            Resultado.Text = Convert.ToString(pensionJubilacion(semanasUsuario, salarioUsuario));
+            if (semanasU >= 250 && semanasU < 350)
+            {
+                pension = 1910;
+            }else if(semanasU >= 350 && semanasU < 449)
+            {
+                pension = 2356;
+            }else if(semanasU >= 450 && semanasU < 549)
+            {
+                pension = 2884;
+            }else if(semanasU >= 550 && semanasU < 649)
+            {
+                pension = 3290;
+            }else if(semanasU >= 650 && semanasU < 749)
+            {
+                pension = 3656;
+            }
+            
+            return pension;
         }
 
         public double pensionJubilacion(int semanasU, double salario)
@@ -41,11 +84,14 @@ namespace CalculadoraINSS
             if (pension < 5000)
             {
                 pension = 5000;
+            }else if(pension >= 53.595)
+            {
+                pension = 53.595;
             }
             
 
 
-            return pension;
+            return Math.Round(pension,2);
         }
     }
 }
